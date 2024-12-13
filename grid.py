fd24106f70958560185d39c2ed4b49a2bb3f301b
Albert Zeng <tcf5pw@virginia.edu>
2024-12-12 20:36:48 -0500

    add wilson algorithm

--- grid.py
+++ grid.py
@@ -7,7 +7,7 @@ class Grid():
         self.generate_grid(generator)
     
     def generate_grid(self, generator):
-        if generator == 'backtrack':
+        if generator == 'bfs':
             self.grid = [[True] * (self.height+2) for _ in range(self.width+2)]
             x, y = 1, 1
             self.grid[x][y] = False
@@ -27,6 +27,52 @@ class Grid():
                     self.grid[(x+nx)//2][(y+ny)//2] = False
                     stack.append((x, y))
                     stack.append((nx, ny))
+        
+        elif generator == 'wilson':
+            self.grid = [[True] * (self.height+2) for _ in range(self.width+2)]
+
+            seen = set()
+            unseen = []
+            for x in range(1, self.width + 1, 2):
+                for y in range(1, self.height + 1, 2):
+                    unseen.append((x, y))
+            
+            x, y = unseen.pop(random.randint(0, len(unseen)-1))
+            seen.add((x, y))
+            self.grid[x][y] = False
+            
+            while len(unseen) > 0:
+                path = {}
+                start = random.choice(unseen)
+                current = start
+                while current not in seen:
+                    x, y = current
+                    neighbors = []
+                    for dx, dy in [(0, -2), (0, 2), (-2, 0), (2, 0)]:
+                        nx = x+dx
+                        ny = y+dy
+                        if 0 < nx < self.width+1 and 0 < ny < self.height+1:
+                            neighbors.append((nx, ny))
+                    if neighbors:
+                        dxy = random.choice(neighbors)
+                        path[current] = dxy
+                        nx, ny = dxy
+                        current = (nx, ny)
+                    else:
+                        self.grid = 'An error occurred during maze generation'
+                        return
+
+                current = start
+                while current not in seen:
+                    x, y = current
+                    seen.add(current)
+                    unseen.remove(current)
+
+                    self.grid[x][y] = False
+                    current = path[current]
+                    nx, ny = current
+                    self.grid[(x+nx)//2][(y+ny)//2] = False
+        
         else:
             self.grid = [
                 [
@@ -38,3 +84,13 @@ class Grid():
 
     def __getitem__(self, coordinate):
         return self.grid[coordinate[0] + 1][coordinate[1] + 1]
+
+g = Grid(400, 400, 'bfs')
+for row in range(g.width+2):
+    l = []
+    for col in range(g.height+2):
+        if g.grid[row][col]:
+            l.append('#')
+        else:
+            l.append(' ')
+    print(''.join(l))
